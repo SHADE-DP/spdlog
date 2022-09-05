70b69a65418e1511f63dbaf307fd78396646a66d
Sri Sham Haran <s.srishamharan@digipen.edu>
2022-09-05 19:49:00 +0800

    added premake

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/*.h",
+		"include/*.hpp",
+		"src/*.cpp"
+	}
+
+	includedirs
+	{
+	  "%{prj.location}/include",
+	}
+
+	links
+	{
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
